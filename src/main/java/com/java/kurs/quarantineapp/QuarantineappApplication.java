088f3af6333f419919fa251865280ffaf84c3a02
Marta Kornaszewska <marta.kornaszewska@orange.com>
2020-03-28 18:59:33 +0100

    Init files

---
+++ src/main/java/com/java/kurs/quarantineapp/QuarantineappApplication.java
@@ -0,0 +1,13 @@
+package com.java.kurs.quarantineapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuarantineappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuarantineappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/kurs/quarantineapp/QuarantineappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.kurs.quarantineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuarantineappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
